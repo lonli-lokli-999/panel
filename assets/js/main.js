c6213673880c47a856454f7765a6f61359bff688
reader <eader-999@yandex.ua>
2019-09-26 18:36:59 +0300

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,107 +1,236 @@
-// ==UserScript==
-// @name     add panel
-// @version  1
-// @grant    none
-// ==/UserScript==
+	'use strict';
 
-'use strict';
-
-/* constants 
-===========================*/
-
-const
-	__styles = 'file:///home/reader/Templates/panel/assets/css/style.css';
-
-/* modules 
+/*===========================
+	panel
 =========================== */
 
-// module calc
-//===========================
-function calc() { 
-  	let
-      calc_box = document.createElement( 'div' );
-    
-    	calc_box.innerHTML = '<h2>Calc</h2><hr><input class="calc_inp" placeholder="write math"><span class="resum"></span>';
+var panel = 
+{
+	render() 
+	{
+		let
+			panel_wrap = document.createElement( 'div' );
+				panel_wrap.classList.add( 'panel-wrap' );
+					panel_wrap.innerHTML = '<div class="b-panel"></div>';
+		document.body.appendChild( panel_wrap );
 
-    	calc_box.querySelector( 'input' )
-        	.addEventListener( 'input',  resum );
+		this.panel_wrap = panel_wrap;		
 
-	function resum(){
-		let val = document.querySelector( '.calc_inp' ).value; 	
-		val != '' ? ( log( eval( val ) ) ) : ( log( '' ) );
-	};
+		document.addEventListener( 'keydown', function( ev ) {
+			if ( ev.shiftKey && ev.code == 'Space' ) {
+				ev.preventDefault();
+				panel.switched();
+			}
+		} );
 
-	function  log( val ){
-		document.querySelector( '.resum' ).innerHTML = val;
-	};
 
-	return calc_box;
-};
-
-// module notes
-//===========================
-var notes = {
-	render(){ 
-		this.notes_box = document.createElement( 'div' ),
-		this.note_list = JSON.parse( localStorage.getItem('msg') ) || [];
-		
-		this.notes_box.innerHTML = '<h2>Notes module</h2><hr><input placeholder="Write note title"><textarea placeholder="Write note"></textarea><button class="save-note">Save</button><div class="note-list"></div>';
-
-		this.notes_box.querySelector( '.save-note' )
-			.addEventListener( 'click', notes.save )
-    
-    return this.notes_box;
-  },
-	save(  ) {
-			let 
-				note_title = notes.notes_box.querySelector( 'input' ),
-				note_msg = notes.notes_box.querySelector( 'textarea' );
-
-			notes.note_list.push( { title: note_title.value, msg: note_msg.value, time: getTime() } );
-      localStorage.setItem('msg', JSON.stringify( notes.note_list ) );
-      
-			notes.reload()
-
-			note_title.value = note_msg.value = '';
+		return this;
 	},
-	reload(){
-			let note_list_html = this.note_list.map( note => `<div class="note"><h3>${ note.title }</h3><p>${ note.msg }</p><p>${ note.time }</p></div>` ).join( '' )
-			this.notes_box.querySelector( '.note-list' ).innerHTML = note_list_html;	
+
+	switched()
+	{
+		this.panel_wrap.classList.toggle( 'panel-wrap--active' )
+	},
+
+	add( aplet ) 
+	{
+		this
+			.panel_wrap
+				.querySelector( '.b-panel' )
+					.appendChild( aplet );
+		return this
 	}
 };
 
-/* functions 
+/* modules
 =========================== */
 
-function add_my_style(){
-  let
-  styles = document.createElement( 'link' );
-  styles.setAttribute( 'rel', 'stylesheet' );
-  styles.setAttribute( 'href', __styles );
+var villages_maneger = 
+{
+	render()
+	{
+		let villages = this.dataLoad();
 
-  document.head.appendChild( styles )
+		let b_villages = document.createElement( 'div' );
+			b_villages.classList.add( 'villages' );
+			b_villages.innerHTML = `<h2>Деревни</h2><hr><div class="villages-list">${this.villages_list_render(villages)}</div>`;
+
+		return b_villages
+	},
+
+	reload()
+	{
+		let
+			b_villages = document.querySelector( '.b-panel .villages' ),
+			b_villages_new =  this.render();
+
+			b_villages.innerHTML = b_villages_new.innerHTML;
+	},
+
+	villages_list_render( villages )
+	{
+		return villages
+			.map( village => `<div class="village" ><a href="${ village.href }">${ village.label }</a><div>Дерево: ${ village.wood }; Глина:${ village.stone }; Сталь:${ village.iron }</div><div class="village__builds"> ${ builds.getBuildsInfo(village.id) } </div</div>` )
+				.join( '' );
+	},
+
+	dataLoad()
+	{
+    	let 
+			game_villages_links = getPageInBox( url_map.villages ), 
+			villages = [];
+
+		game_villages_links = game_villages_links.querySelectorAll( '#production_table .nowrap' );
+		
+		game_villages_links.forEach( function( village )
+			{
+				
+				let village_link = village.querySelector( '.quickedit-content a:not(.rename-icon)' ),
+					res_wood = village.querySelector( '.wood' ).innerHTML,
+					res_stone = village.querySelector( '.stone' ).innerHTML,
+					res_iron = village.querySelector( '.iron' ).innerHTML,
+					village_id = village.querySelector( '.quickedit-vn' ).getAttribute( 'data-id' );
+
+				villages.push( 
+					{
+						href: village_link.href,
+						label: village_link.innerHTML,
+						wood: res_wood,
+						stone: res_stone,
+						iron: res_iron,
+						id: village_id
+					} 
+				)
+			}
+		);
+		
+		return villages
+	}
 };
 
-function panel_render(){
-	let panel = document.createElement( 'div' );
-		panel.classList.add( 'panel' );
+var	builds =
+{
+	getBuildsInfo( id )
+	{
+		let 
+			builds_html = this.getBuildsHtml( id ),
+			builds_info = this.buildsInfoExtract( builds_html );
 
-		panel.add = ( aplet ) => ( panel.appendChild( aplet ) );
-	return document.body.appendChild( panel );
+		return builds_info ? this.render( builds_info ) : '';
+	},
+
+	getBuildsHtml( id )
+	{
+		let
+			village_build_page = url_map.villageBuilds( id );
+
+		return getPageInBox( village_build_page ).querySelectorAll( '[class*=" buildorder"]' )
+	},
+
+	buildsInfoExtract( builds )
+	{
+		builds.map = [].map;
+		let builds_info = builds.map( function( build )
+			{
+				return {
+					name: build.querySelectorAll( '.lit-item' )[0].innerText,
+					time: build.querySelectorAll( '.lit-item' )[1].innerText
+				}
+			}
+		);
+
+		return builds_info
+	},
+	
+	render( builds )
+	{
+		return builds.map( build => `<span>${ build.name }; Завершение: ${ build.time };</span>` ).join( '' )
+	}
 };
 
-function getTime(){
-	let time = new Date();
-	return `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`;
+var bookmarks =
+{
+	render()
+	{
+		let 
+			b_bookmarks = document.createElement( 'div' ),
+			bookmarks_html = `<a href="${ url_map.areaPage }">Площадь</a><a href="${ url_map.villageBuilds() }">Ратуша</a>`;
+
+		b_bookmarks.classList.add( 'bookmarks' );
+		b_bookmarks.innerHTML = bookmarks_html;
+		
+		return b_bookmarks
+	}
 }
 
-(function(){
-	// load script and style
-	add_my_style();
+/* map
+===========================*/
+const url_map = 
+{
+	get location( )
+	{
+		return window.location
+	},
 
-	// render html
-	let panel = panel_render();
-		panel.add( calc() )
-		panel.add( notes.render() );
-  	notes.reload();
-}( null ));
+	get villages() 
+	{
+		let host = this.location;
+		return `${ host.origin }${ host.pathname }?screen=overview_villages`
+	},
+	
+	get idCurrentVillage()
+	{
+		let host = this.location,
+		search_obj = new URLSearchParams( host );
+
+		return search_obj.get( 'village' )
+	},
+
+	villageBuilds( id )
+	{
+		let host = this.location;
+		return `${ host.origin }${ host.pathname }?village=${id || this.idCurrentVillage }&screen=main`
+	},
+
+	get areaPage()
+	{
+		let host = this.location;
+		return `${ host.origin }${ host.pathname }?village=${ this.idCurrentVillage }&screen=place`
+	}
+};
+
+/* function
+=========================== */
+function requestSync( url )
+{
+        let request = new XMLHttpRequest();
+            request.open('GET', url, false);
+            request.send();
+        return request.status != 200 ? `${ request.status }: ${ request.statusText }` : request.responseText;  
+};
+
+function getPageInBox(page_url, select ) {
+    let resBox = document.createElement( 'div' );
+        resBox.innerHTML = requestSync( page_url );
+
+	return select ? resBox.querySelector( select ) : resBox;
+};
+
+function watcher()
+{
+	villages_maneger.reload()		
+};
+
+/* run 
+=========================== */
+
+(function() {
+		panel.render()
+			.add( bookmarks.render() )
+			.add( villages_maneger.render() )		
+		; 
+
+		setInterval( watcher, 3000 )
+} ( null ));
+
+
---
+++ dist/build.js
@@ -0,0 +1,237 @@
+	'use strict';
+
+/*===========================
+	panel
+=========================== */
+
+var panel = 
+{
+	render() 
+	{
+		let
+			panel_wrap = document.createElement( 'div' );
+				panel_wrap.classList.add( 'panel-wrap' );
+					panel_wrap.innerHTML = '<div class="b-panel"></div>';
+		document.body.appendChild( panel_wrap );
+
+		this.panel_wrap = panel_wrap;		
+
+		document.addEventListener( 'keydown', function( ev ) {
+			if ( ev.shiftKey && ev.code == 'Space' ) {
+				ev.preventDefault();
+				panel.switched();
+			}
+		} );
+
+
+		return this;
+	},
+
+	switched()
+	{
+		this.panel_wrap.classList.toggle( 'panel-wrap--active' )
+	},
+
+	add( aplet ) 
+	{
+		this
+			.panel_wrap
+				.querySelector( '.b-panel' )
+					.appendChild( aplet );
+		return this
+	}
+};
+
+/* modules
+=========================== */
+
+var villages_maneger = 
+{
+	render()
+	{
+		let villages = this.dataLoad();
+
+		let b_villages = document.createElement( 'div' );
+			b_villages.classList.add( 'villages' );
+			b_villages.innerHTML = `<h2>Деревни</h2><hr><div class="villages-list">${this.villages_list_render(villages)}</div>`;
+
+		return b_villages
+	},
+
+	reload()
+	{
+		let
+			b_villages = document.querySelector( '.b-panel .villages' ),
+			b_villages_new =  this.render();
+
+			b_villages.innerHTML = b_villages_new.innerHTML;
+	},
+
+	villages_list_render( villages )
+	{
+		return villages
+			.map( village => `<div class="village" ><a href="${ village.href }">${ village.label }</a><div>Дерево: ${ village.wood }; Глина:${ village.stone }; Сталь:${ village.iron }</div><div class="village__builds"> ${ builds.getBuildsInfo(village.id) } </div</div>` )
+				.join( '' );
+	},
+
+	dataLoad()
+	{
+    	let 
+			game_villages_links = getPageInBox( url_map.villages ), 
+			villages = [];
+
+		game_villages_links = game_villages_links.querySelectorAll( '#production_table .nowrap' );
+		
+		game_villages_links.forEach( function( village )
+			{
+				
+				let village_link = village.querySelector( '.quickedit-content a:not(.rename-icon)' ),
+					res_wood = village.querySelector( '.wood' ).innerHTML,
+					res_stone = village.querySelector( '.stone' ).innerHTML,
+					res_iron = village.querySelector( '.iron' ).innerHTML,
+					village_id = village.querySelector( '.quickedit-vn' ).getAttribute( 'data-id' );
+
+				villages.push( 
+					{
+						href: village_link.href,
+						label: village_link.innerHTML,
+						wood: res_wood,
+						stone: res_stone,
+						iron: res_iron,
+						id: village_id
+					} 
+				)
+			}
+		);
+		
+		return villages
+	}
+};
+
+var	builds =
+{
+	getBuildsInfo( id )
+	{
+		let 
+			builds_html = this.getBuildsHtml( id ),
+			builds_info = this.buildsInfoExtract( builds_html );
+
+		return builds_info ? this.render( builds_info ) : '';
+	},
+
+	getBuildsHtml( id )
+	{
+		let
+			village_build_page = url_map.villageBuilds( id );
+
+		return getPageInBox( village_build_page ).querySelectorAll( '[class*=" buildorder"]' )
+	},
+
+	buildsInfoExtract( builds )
+	{
+		builds.map = [].map;
+		let builds_info = builds.map( function( build )
+			{
+				return {
+					name: build.querySelectorAll( '.lit-item' )[0].innerText,
+					time: build.querySelectorAll( '.lit-item' )[1].innerText
+				}
+			}
+		);
+
+		return builds_info
+	},
+	
+	render( builds )
+	{
+		return builds.map( build => `<span>${ build.name }; Завершение: ${ build.time };</span>` ).join( '' )
+	}
+};
+
+var bookmarks =
+{
+	render()
+	{
+		let 
+			b_bookmarks = document.createElement( 'div' ),
+			bookmarks_html = `<a href="${ url_map.areaPage }">Площадь</a><a href="${ url_map.villageBuilds() }">Ратуша</a>`;
+
+		b_bookmarks.classList.add( 'bookmarks' );
+		b_bookmarks.innerHTML = bookmarks_html;
+		
+		return b_bookmarks
+	}
+}
+
+/* map
+===========================*/
+const url_map = 
+{
+	get location( )
+	{
+		return window.location
+	},
+
+	get villages() 
+	{
+		let host = this.location;
+		return `${ host.origin }${ host.pathname }?screen=overview_villages`
+	},
+	
+	get idCurrentVillage()
+	{
+		let host = this.location,
+		search_obj = new URLSearchParams( host );
+
+		return search_obj.get( 'village' )
+	},
+
+	villageBuilds( id )
+	{
+		let host = this.location;
+		return `${ host.origin }${ host.pathname }?village=${id || this.idCurrentVillage }&screen=main`
+	},
+
+	get areaPage()
+	{
+		let host = this.location;
+		return `${ host.origin }${ host.pathname }?village=${ this.idCurrentVillage }&screen=place`
+	}
+};
+
+/* function
+=========================== */
+function requestSync( url )
+{
+        let request = new XMLHttpRequest();
+            request.open('GET', url, false);
+            request.send();
+        return request.status != 200 ? `${ request.status }: ${ request.statusText }` : request.responseText;  
+};
+
+function getPageInBox(page_url, select ) {
+    let resBox = document.createElement( 'div' );
+        resBox.innerHTML = requestSync( page_url );
+
+	return select ? resBox.querySelector( select ) : resBox;
+};
+
+function watcher()
+{
+	villages_maneger.reload()		
+};
+
+/* run 
+=========================== */
+
+(function() {
+		panel.render()
+			.add( bookmarks.render() )
+			.add( villages_maneger.render() )		
+		; 
+
+		setInterval( watcher, 3000 )
+} ( null ));
+
+
+(function(){let styles = document.createElement( 'style' );styles.innerHTML = ':root {--main-bg: #fff;--main-color: #999;--dbl-bg: #eee;--dbl-color: #977;}.panel-wrap {position: fixed;top: 0;left: 0;background: var( --main-bg );box-shadow: 0 0 .5rem var( --main-color );height: 100%;transition: 500ms;  width: .2rem;  opacity: 0; overflow-x: hidden;overflow-y: auto;  z-index: 12324242;}.panel-wrap:hover,.panel-wrap--active {width: 30rem;opacity: 1;}.b-panel,.b-panel * {border: 0;padding: 0;margin:0;outline: none;font-size: 16px;box-sizing: border-box;color: var( --main-color );}.b-panel h2,.b-panel h3,.b-panel p {  padding: .5rem 1rem;}.b-panel hr {border: .1rem solid var( --main-color );margin: .5rem 0;}.b-panel input,.b-panel textarea,.b-panel button {display: block;width: 100%;background: var( --main-bg );padding: 1rem;margin: .5rem 0;transition: 500ms;}.b-panel input:hover,.b-panel textarea:hover,.b-panel button:hover {background: var( --dbl-bg );}.b-panel a {font-style: italic;transition: 500ms;text-decoration: none;}.b-panel a:hover {color: var( --main-color )}.villages-list {margin: .5rem 0;}.villages-list .village {padding: .5rem;}.villages-list .village:nth-child(even) {background: var( --dbl-bg );}.village__builds span {display: block;padding: .2rem 0;}.bookmarks {padding: .5rem 0;}.bookmarks a {display: inline-block;padding: .5rem;}.bookmarks a:hover {background: var( --dbl-bg );}';document.head.appendChild( styles )}(null))
